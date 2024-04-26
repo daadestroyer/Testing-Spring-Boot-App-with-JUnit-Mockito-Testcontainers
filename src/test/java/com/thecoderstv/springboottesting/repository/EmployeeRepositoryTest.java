b314bb958dffdc2a43846ed695bbf11b712a95c4
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-04-26 12:43:20 +0530

    Junit test for get all employee operation
    Junit test for get all employee operation

--- src/test/java/com/thecoderstv/springboottesting/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/thecoderstv/springboottesting/repository/EmployeeRepositoryTest.java
@@ -4,11 +4,15 @@ package com.thecoderstv.springboottesting.repository;
 import com.thecoderstv.springboottesting.Model.Employee;
 import com.thecoderstv.springboottesting.Repository.EmployeeRepository;
 
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 
 
 // this annotation loads only repository layer components
@@ -21,6 +25,8 @@ public class EmployeeRepositoryTest {
 
     // Junit testcase for save employee operation
     // following given_when_then condition
+
+    @DisplayName("Junit test for save employee operation")
     @Test
     public void givenEmployeeObject_whenSave_thenReturnSavedEmployee() {
         // given  - precondition or setup or sample data
@@ -30,8 +36,37 @@ public class EmployeeRepositoryTest {
                 .email("nigamshubhamxxx@gmail.com")
                 .build();
 
+        // when - action or behaviour
         Employee savedEmployee = employeeRepository.save(employee);
 
+        // then - verify the output
         Assertions.assertThat(savedEmployee.getId()).isEqualTo(employee.getId());
+    }
+
+    @DisplayName("Junit test for get all employee operation")
+    @Test
+    public void givenEmployeeList_whenFindAll_thenEmployeeList() {
+        // given - precondition setup
+        Employee employee1 = Employee.builder()
+                .firstName("Shubham")
+                .lastName("Nigam")
+                .email("nigamshubhamxxx@gmail.com")
+                .build();
+
+        Employee employee2 = Employee.builder()
+                .firstName("Ram")
+                .lastName("Nigam")
+                .email("ramnigam@gmail.com")
+                .build();
+
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+
+        // when - action or behaviour
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        // then - verify the output
+        Assertions.assertThat(employeeList).isNotNull();
+        Assertions.assertThat(employeeList.size()).isEqualTo(2);
     }
 }
