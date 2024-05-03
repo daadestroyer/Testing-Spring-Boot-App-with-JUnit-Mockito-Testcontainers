d604f5789110720d28668dccf13e5d6e88bd6fda
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-05-03 17:44:52 +0530

    JUnit testcase for Employee Controller to get all employee
    JUnit testcase for Employee Controller to get all employee

--- src/test/java/com/thecoderstv/springboottesting/controller/EmployeeControllerTest.java
+++ src/test/java/com/thecoderstv/springboottesting/controller/EmployeeControllerTest.java
@@ -5,22 +5,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thecoderstv.springboottesting.Model.Employee;
 import com.thecoderstv.springboottesting.Service.EmployeeService;
 import com.thecoderstv.springboottesting.Service.Impl.EmployeeServiceImpl;
+import lombok.Builder;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatcher;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
+
 import static org.mockito.Mockito.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcResultMatchersDsl;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 @WebMvcTest
 public class EmployeeControllerTest {
@@ -34,12 +40,12 @@ public class EmployeeControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @DisplayName("JUnit Controller test to save employee")
+    @DisplayName("JUnit testcase for Employee Controller to save employee")
     @Test
     public void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
         // given - precondition setup
         Employee employee = Employee.builder().firstName("Shubham").lastName("Nigam").email("shubham@gmail.com").build();
-        when(employeeService.saveEmployee(ArgumentMatchers.any())).thenAnswer((invocation)->invocation.getArgument(0));
+        when(employeeService.saveEmployee(ArgumentMatchers.any())).thenAnswer((invocation) -> invocation.getArgument(0));
 
         // when - action or behaviour
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders.post("/api/employees/create-employee")
@@ -53,6 +59,26 @@ public class EmployeeControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Shubham"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Nigam"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("shubham@gmail.com"));
+    }
+
+    @DisplayName("JUnit testcase for Employee Controller to get all employee")
+    @Test
+    public void givenListOfEmployee_whenGetAllEmployee_thenReturnEmployeeList() throws Exception {
+        // given - precondition setup
+        List<Employee> employeeList = List.of(
+                Employee.builder().firstName("Shubham").lastName("Nigam").email("shubham@gmail.com").build(), Employee.builder().firstName("Ram").lastName("Singh").email("ram@gmail.com").build()
+        );
+        when(employeeService.getAllEmployees()).thenReturn(employeeList);
+
+        // when - action or behaviour
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/employees/get-all-employee"));
+
+        // then - verify the output
+        response
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()",CoreMatchers.is(employeeList.size())));
+
     }
 
 }
